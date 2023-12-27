f9d34da6c23880563c0e39c6920b01b4cb242e47
momenalabsy <125696838+momenalabsy@users.noreply.github.com>
2023-12-28 00:33:12 +0200

    Upload Book Library project
    This commit includes the initial version of the Book Library project. Users can add and remove books, and the library dynamically updates to reflect changes.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Library-TOP
+
+# Overview
+
+This simple web application allows users to input information about a book, including its title, author, and the number of pages. Upon submission, this information is used to create a new book object, which is then added to the library. The library is dynamically updated to display the added books.
+
+# Usage
+
+1.Enter Book Information:
+
+Provide the title, author, and number of pages for the book in the respective input fields.
+
+2.Add Book:
+
+Click the "Add" button to submit the information. This action creates a new book object, adding it to both the dynamically updated library and the Book Library data structure.
+
+3.Remove Book:
+
+Each book in the library is accompanied by a "Remove" button. Clicking this button removes the book from both the DOM and the Book Library data structure.
+
+# How It Works
+
+The application utilizes HTML, CSS, and JavaScript to create an interactive interface for managing a virtual book library.
+
+Each book is represented as an object with properties such as title, author, and pages.
+
+The "Remove" button triggers the removal of a book from both the display and the underlying library data.
---
+++ script.js
@@ -0,0 +1,63 @@
+// get HTML elements
+let title = document.getElementById("title");
+let author = document.getElementById("author");
+let pages = document.getElementById("pages");
+let add = document.getElementById("add");
+let clear = document.getElementById("clear");
+let form = document.getElementById("book-form");
+let booksContainer = document.getElementById("books-container");
+// removeButton = document.getElementsByClassName("remove");
+let book = document.getElementsByClassName("book");
+// adding the logic and functions
+
+const myLibrary = [];
+
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+function addBookToThePage() {
+  let container = document.createElement("div");
+  let remove = document.createElement("button");
+
+  let p1 = document.createElement("p");
+  p1.textContent = `Title: ${title.value}`;
+  let p2 = document.createElement("p");
+  p2.textContent = `Author: ${author.value}`;
+  let p3 = document.createElement("p");
+  p3.textContent = `Pages: ${pages.value}`;
+
+  remove.textContent = "X";
+  remove.className = "remove";
+  container.className = "book";
+  container.append(p1);
+  container.append(remove);
+  container.append(p2);
+  container.append(p3);
+  booksContainer.append(container);
+}
+function addBookToLibrary(event) {
+  event.preventDefault();
+  const newBook = new Book(title.value, author.value, pages.value);
+  myLibrary.push(newBook);
+  addBookToThePage();
+  form.reset();
+
+  booksContainer.addEventListener("click", function (event) {
+    if (event.target.classList.contains("remove")) {
+      const removeButton = event.target;
+      const bookContainer = removeButton.parentElement;
+      const bookIndex = Array.from(booksContainer.children).indexOf(
+        bookContainer
+      );
+
+      if (bookIndex !== -1) {
+        myLibrary.splice(bookIndex, 1);
+      }
+      bookContainer.remove();
+    }
+  });
+}
+form.addEventListener("submit", addBookToLibrary);
